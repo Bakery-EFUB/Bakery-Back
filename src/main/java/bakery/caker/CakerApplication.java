941f0286298876b98bc32d8b811d11f4c2981c82
KJI-DEC <kji_dec22@ewhain.net>
2022-07-11 22:29:57 +0900

    initial commit

---
+++ src/main/java/bakery/caker/CakerApplication.java
@@ -0,0 +1,13 @@
+package bakery.caker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bakery/caker/CakerApplicationTests.java
@@ -0,0 +1,13 @@
+package bakery.caker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
