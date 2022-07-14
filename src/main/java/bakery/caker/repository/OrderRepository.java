24ac63e3e4e40eb983bb74d5146c80ff0b8bf547
KJI-DEC <kji_dec22@ewhain.net>
2022-07-15 01:35:00 +0900

    Chore: Member import
    member import
    
    related to #2

--- src/main/java/bakery/caker/repository/OrderRepository.java
+++ src/main/java/bakery/caker/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package bakery.caker.repository;
 
+import bakery.caker.domain.Member;
 import bakery.caker.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
