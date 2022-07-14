dca34afcf366166a42d20cb8eff59e4181830612
KJI-DEC <kji_dec22@ewhain.net>
2022-07-14 22:32:46 +0900

    New: order domain 생성
    1. order entity 생성
    2. order repository 생성
    
    related to #2

---
+++ src/main/java/bakery/caker/domain/Order.java
@@ -0,0 +1,55 @@
+package bakery.caker.domain;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    private String locationGu;
+    private String locationDong;
+    private String type;
+    private String size;
+    private String flavor;
+
+    @Column(columnDefinition= "TEXT")
+    private String description;
+
+    private String image;
+    private LocalDateTime pickupDate;
+    private Integer priceMin;
+    private Integer priceMax;
+    private LocalDateTime createdAt;
+    private String hashtag;
+    private Boolean finishedFlag;
+
+    @ManyToOne()
+    @JoinColumn(name = "member_id")
+    private Member orderedBy;
+
+    @Builder
+    public Order(String locationGu, String locationDong, String type, String size, String flavor, String description, String image, LocalDateTime pickupDate, Integer priceMin, Integer priceMax, String hashtag, Member orderedBy){
+        this.locationGu = locationGu;
+        this.locationDong = locationDong;
+        this.type = type;
+        this.size = size;
+        this.flavor = flavor;
+        this.description = description;
+        this.image = image;
+        this.pickupDate = pickupDate;
+        this.priceMin = priceMin;
+        this.priceMax = priceMax;
+        this.orderedBy = orderedBy;
+        this.hashtag = hashtag;
+        this.createdAt = LocalDateTime.now();
+        this.finishedFlag = false;
+    }
+}
+
---
+++ src/main/java/bakery/caker/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package bakery.caker.repository;
+
+import bakery.caker.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByFinishedFlag(Boolean finishedFlag);
+    List<Order> findAllByLocationGuAndLocationDongAndFinishedFlag(String locationGu, String locationDong, Boolean finishedFlag);
+    List<Order> findAllByFinishedFlagOrderByCreatedAtDesc(Boolean finishedFlag);
+}
