3d614c778cffff513812cad61840e7b533777902
KJI-DEC <kji_dec22@ewhain.net>
2022-07-17 17:28:22 +0900

    Fix: build 패턴 변경
    생성자 주입 -> build 패턴으로 변경
    
    related to #4

--- src/main/java/bakery/caker/dto/OrderResponseDTO.java
+++ src/main/java/bakery/caker/dto/OrderResponseDTO.java
@@ -1,7 +1,7 @@
 package bakery.caker.dto;
 
 import bakery.caker.domain.Order;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,7 +9,11 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class OrderResponseDTO {
     List<Order> orderList;
+
+    @Builder
+    public OrderResponseDTO(List<Order> orderList){
+        this.orderList = orderList;
+    }
 }
