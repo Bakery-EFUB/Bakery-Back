c4106282d3ddde119cc6207b182d16c4a0817abd
KJI-DEC <kji_dec22@ewhain.net>
2022-07-17 17:51:18 +0900

    New: Comment 도메인 추가
    1. Comment entity
    2. CommentRepository
    3. Recomment entity
    4. RecommentRepository
    
    related to #2

---
+++ src/main/java/bakery/caker/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package bakery.caker.repository;
+
+import bakery.caker.domain.Comment;
+import bakery.caker.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByWriter(Member writer);
+}
---
+++ src/main/java/bakery/caker/repository/RecommentRepository.java
@@ -0,0 +1,13 @@
+package bakery.caker.repository;
+
+import bakery.caker.domain.Member;
+import bakery.caker.domain.Recomment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecommentRepository extends JpaRepository<Recomment, Long> {
+    List<Recomment> findAllByWriter(Member writer);
+}
