5e465a8087550df9b3d4b84d11e32c306bfe7bbc
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-07-25 13:00:00 +0900

    Fix: cors 문제 해결 시도2

--- src/main/java/bakery/caker/config/CORSConfig.java
+++ src/main/java/bakery/caker/config/CORSConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RequiredArgsConstructor
 @Configuration
 public class CORSConfig implements WebMvcConfigurer {
-    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
